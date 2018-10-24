ac163194d4bc6b7afd603de9a95326e2d8fd1330
duikerdddd <824354305@qq.com>
2018-10-25 00:04:12 +0800

    study

---
+++ ReverseString.c
@@ -0,0 +1,29 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str++)
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//void ReverseString(char*str)
+//{
+//	if (*str){
+//		char tmp;
+//		int len = strlen(str);
+//		tmp = str[0];
+//		str[0] = str[len - 1];
+//		str[len - 1] = '\0';
+//		ReverseString(str + 1);
+//		str[len - 1] = tmp;
+//	}
+//}
+//int main()
+//{
+//	char str[] = "you love me";
+//	ReverseString(str);
+//	system("pause");
+//}

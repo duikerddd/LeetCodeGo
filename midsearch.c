c5437b4cc765cbecc57a81d25ff5aa0d926ebe0b
duikerddd <44222023+duikerddd@users.noreply.github.com>
2018-10-25 19:07:26 +0800

    Update midsearch.c

--- midsearch.c
+++ midsearch.c
@@ -1,33 +1,33 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//int main()
-//{
-//	int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//	int left = 0;
-//	int right = sizeof(a) / sizeof(a[0])-1;
-//	int target = 0;
-//	printf("ÇëÊäÈëÄãÏë²éµÄÊý×Ö:");
-//	scanf("%d", &target);
-//	while (left<=right){
-//		int mid = (left + right) / 2;
-//		if (mid > target)
-//		{
-//			right = mid - 1;
-//		}
-//		else if (mid < target)
-//		{
-//			left = mid + 1;
-//		}
-//		else if (mid == target)
-//		{
-//			printf("Ä¿±êº¯ÊýÏÂ±êÊÇ:%d\n",mid);
-//			break;
-//		}
-//	}
-//	if (right < left)
-//	{
-//		printf("ÄãÏë²éµÄÊý×ÖÃ»ÓÐÕÒµ½!\n");
-//	}
-//	system("pause");
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int left = 0;
+	int right = sizeof(a) / sizeof(a[0])-1;
+	int target = 0;
+	printf("è¯·è¾“å…¥ä½ æƒ³æŸ¥çš„æ•°å­—:");
+	scanf("%d", &target);
+  while (left<=right){
+		int mid = (left + right) / 2;
+		if (mid > target)
+		{
+			right = mid - 1;
+		}
+		else if (mid < target)
+		{
+			left = mid + 1;
+		}
+		else if (mid == target)
+		{
+			printf("ç›®æ ‡å‡½æ•°ä¸‹æ ‡æ˜¯:%d\n",mid);
+			break;
+		}
+	}
+	if (right < left)
+	{
+		printf("ä½ æƒ³æŸ¥çš„æ•°å­—æ²¡æœ‰æ‰¾åˆ°!\n");
+	}
+	system("pause");
+}

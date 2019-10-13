455f5640cd79e7e5d537f5cbcfb5c875fcbb643e
duikerddd <44222023+duikerddd@users.noreply.github.com>
2019-10-13 18:29:25 +0800

    Update egg_drop.cpp

--- egg_drop.cpp
+++ egg_drop.cpp
@@ -16,20 +16,22 @@ int min(int a,int b){
     return ans;
 }
 
-int egg(int n,int h){
-
+int egg(int k,int n){
     //Basis case
     //一个鸡蛋,最坏情况下最少尝试h次
-    if(n==1) return h;
+    if(k==1) return h;
     //没有楼层,不需要尝试
-    if(h==0) return 0;
+    if(n==0) return 0;
     //一层一次就够
-    if(h==1) return 1;
+    if(n==1) return 1;
 
     int minimum=INT_MAX;
 
     //从1,2,3...层开始扔,寻找所有最坏情况里的最小值
-    for(int x=1;x<=h;x++) minimum=min(minimum,(1+max(egg(n,h-x),egg(n-1,x-1))));
-
+    for(int x=1;x<=n;x++) 
+    {
+        //鸡蛋没碎,层数为n-x层   鸡蛋碎了,层数为x-1层
+        minimum=min (minimum,  (1+max(egg(k,n-x),egg(k-1,x-1))) );
+    }
     return minimum;
 }

131543d222e81df25f74e1f4567b383e34e18fe8
duikerddd <44222023+duikerddd@users.noreply.github.com>
2019-10-25 14:25:05 +0800

    Update two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -16,3 +16,25 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    //使用map存储每个元素和索引值,用差值寻找到则返回,没找到则插入该元素和索引值
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> record;
+        vector<int> ret;
+        for(int i=0;i<nums.size();++i)
+        {
+            int complement = target - nums[i];
+            if(record.find(complement) != record.end()){
+                ret.push_back(record[complement]);
+                ret.push_back(i);
+                break;
+            }
+
+            record[nums[i]] = i;
+
+        }
+        return ret;
+    }
+};

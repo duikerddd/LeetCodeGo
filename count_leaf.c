f923e2ac816c4735c9fa1c1f7ac65e4b90d9129d
duikerddd <44222023+duikerddd@users.noreply.github.com>
2019-10-31 17:51:22 +0800

    Create count_leaf.c

---
+++ count_leaf.c
@@ -0,0 +1,17 @@
+//求结点个数
+//终止条件: 
+//1)空树 return 0
+//2)叶子结点 return 1
+//3)一个孩子为空
+int GetSize(BNode *root)
+{
+	//终止条件
+	if (root == NULL)
+	{
+		//空树
+		return 0;
+	}
+	//递推
+	return GetSize(root->left) +
+		GetSize(root->right) + 1;
+}

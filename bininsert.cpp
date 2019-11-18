027a22791a8ab43d9f560af9f1d909429365b816
duikerddd <44222023+duikerddd@users.noreply.github.com>
2019-11-18 18:43:15 +0800

    Create bininsert.cpp

---
+++ bininsert.cpp
@@ -0,0 +1,12 @@
+class BinInsert {
+public:
+    //n   1024 =  10 0000 0000            10 0000 0000    
+    //m   19 =          1 0011 -->左移2位      100 11
+    //                                    10 0100 1100
+    //m插入到n的j~i位
+    //m左移到i位,直接|,即可插入
+    int binInsert(int n, int m, int j, int i) {
+        m <<= j;
+        return n | m;
+    }
+};

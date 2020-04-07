9c3540ab4503b671acede32421458a9078a4de36
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-04-07 14:50:35 +0800

    Create cut_firest_str.cpp

---
+++ cut_firest_str.cpp
@@ -0,0 +1,19 @@
+string cut_str;
+string& CutFirstStr( string& s1,  string& s2)
+{
+	//无效输入
+	if (s1.empty() || s1.size() == 0) return s1;
+
+	
+	vector<int> cut_pos(256, 0);
+	//标记
+	for (int i = 0; i<s2.size(); ++i)
+		cut_pos[s2[i]]++;
+	//导入
+	for (int i = 0; i<s1.size(); ++i)
+	{
+		if (cut_pos[s1[i]] != 1)
+			cut_str.push_back(s1[i]);
+	}
+	return cut_str;
+}

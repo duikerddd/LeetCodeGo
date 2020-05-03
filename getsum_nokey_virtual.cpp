e09f7f3845f7d1721ce4dce165d713ca68c99df9
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-05-03 21:51:46 +0800

    Create getsum_nokey_virtual.cpp

---
+++ getsum_nokey_virtual.cpp
@@ -0,0 +1,29 @@
+//解法2: 利用多态模拟递归终止条件if,父类虚函数为终止条件,子类虚函数为递加
+class A{
+public:
+	virtual int Sum(int n)
+	{
+		return 0;
+	}
+};
+
+class B : public A{
+public:
+	virtual int Sum(int n)
+	{
+		A a;
+		B b;
+		A* p[2] = { &a, &b };
+		return n + p[!!(n - 1)]->Sum(n-1);  //(!!n) : n!=0 --> n=1  ---> !n=0 --> !!n=1
+											//        n==0 --> n=0  ---> !n=1 --> !!n=0
+	}
+};
+
+class Solution{
+public:
+	int GetSum(int n)
+	{
+		B b;
+		return b.Sum(n);
+	}
+};

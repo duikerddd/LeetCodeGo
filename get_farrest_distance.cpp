2616087fc8a77cb5e92a7bdf86e95cc37a35c32f
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-09-19 18:56:14 +0800

    Update get_farrest_distance.cpp

--- get_farrest_distance.cpp
+++ get_farrest_distance.cpp
@@ -2,19 +2,18 @@
 //前序遍历,回溯时计算
 int GetFarrestDistance(BNode *root)
 {
-  //终止条件: 走到头 
+        //终止条件: 走到头 
 	if (root == NULL)
 	{
 		return 0;
 	}
-
 
 	int leftDistance = GetFarrestDistance(root->left);
 	int rightDistance = GetFarrestDistance(root->right);
 
 	int leftHeight = GetHeight(root->left);
 	int rightHeight = GetHeight(root->right);
-  //记录最
+     //记录最
 	int rootDistance = leftHeight + rightHeight;
 
 	return MAX3(leftDistance, rightDistance, rootDistance);
